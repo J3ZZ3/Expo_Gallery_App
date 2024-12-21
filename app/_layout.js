b7e1d90bbb928aa1225a11da3338677da2a9c928
Jesse <jesse.mashoana@gmail.com>
2024-12-21 22:04:00 +0200

    camera working

---
+++ app/_layout.js
@@ -0,0 +1,34 @@
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { Drawer } from "expo-router/drawer";
+import { Image, ImageBackground, View, Text, SafeAreaView } from "react-native";
+import * as Location from 'expo-location';
+import { useState, useEffect } from 'react';
+import CameraComponent from "../components/CameraComponents";
+
+export default function Layout() {
+
+  const [location, setLocation] = useState('');
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+
+  const getLocation = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      console.error('Permission to access location was denied');
+    }
+    let location = await Location.getCurrentPositionAsync({});
+    setLocation(location);
+  }
+  useEffect(() => {
+    console.log({location: location});
+    
+  }, [location]);
+  
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+    <CameraComponent/>
+    </SafeAreaView>
+  );
+}
